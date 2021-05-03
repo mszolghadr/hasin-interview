25d4105f8a65d03dbb9e0519ac6c12d1290fa9ac
M.Sadegh Zolghadr <ms.zolghadr@gmail.com>
2021-05-03 17:09:58 +0430

    add test project

---
+++ Tests/TestFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using tadbir.Data;
+using tadbir.Entities;
+using tadbir.Repository;
+using Xunit;
+namespace Tests
+{
+    public class TestFixture : IUseFixture<HasinContext>
+    {
+        private static TadbirDbContext _context;
+
+        public TestFixture()
+        {
+        }
+
+        public static TadbirDbContext Context
+        {
+            get
+            {
+                var options = new DbContextOptionsBuilder<TadbirDbContext>()
+                 .UseInMemoryDatabase(databaseName: "InterviewDataBase")
+                 .Options;
+                return _context ??= new TadbirDbContext(options);
+            }
+        }
+    }
+}
